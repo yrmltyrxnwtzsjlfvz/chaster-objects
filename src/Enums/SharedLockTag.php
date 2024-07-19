72ca1192a249852fb39170083952e16e2560f686
yrmltyrxnwtzsjlfvz <fake@knks.xyz>
2024-07-19 09:59:32 -0500

    Added SharedLockTag

---
+++ src/Enums/SharedLockTag.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Fake\ChasterObjects\Enums;
+
+use Bytes\EnumSerializerBundle\Enums\StringBackedEnumInterface;
+use Bytes\EnumSerializerBundle\Enums\StringBackedEnumTrait;
+
+/**
+ * @since 2024-07-19
+ */
+enum SharedLockTag: string implements StringBackedEnumInterface
+{
+    use StringBackedEnumTrait;
+
+    case ANAL = 'Anal';
+    case BONDAGE = 'Bondage';
+    case BRA = 'Bra';
+    case CHASTITY_MONTH = 'Chastity Month';
+    case CHORE = 'Chore';
+    case CLOTHES = 'Clothes';
+    case COLLAR = 'Collar';
+    case CONTRACT = 'Contract';
+    case CONTROL = 'Control';
+    case CUFF = 'Cuff';
+    case DENIAL = 'Denial';
+    case DRESS = 'Dress';
+    case EDGING = 'Edging';
+    case FEMBOY = 'Femboy';
+    case FEMINIZATION = 'Feminization';
+    case FITNESS = 'Fitness';
+    case GAG = 'Gag';
+    case GAMBLE = 'Gamble';
+    case GAME = 'Game';
+    case GIFT = 'Gift';
+    case HEELS = 'Heels';
+    case HUMILIATION = 'Humiliation';
+    case LOCKTOBER = 'Locktober';
+    case LUCK = 'Luck';
+    case NIPPLE = 'Nipple';
+    case NO_NUT_NOVEMBER = 'No Nut November';
+    case ORGASM = 'Orgasm';
+    case PLUG = 'Plug';
+    case PRISON = 'Prison';
+    case PUNISHMENT = 'Punishment';
+    case PUPPY = 'Puppy';
+    case ROPE = 'Rope';
+    case ROULETTE = 'Roulette';
+    case SISSY = 'Sissy';
+    case SLAVE = 'Slave';
+    case SPORT = 'Sport';
+    case TASK = 'Task';
+    case TEASE = 'Tease';
+    case TOY = 'Toy';
+    case TRAINING = 'Training';
+    case TRAP = 'Trap';
+    case WEEKEND = 'Weekend';
+}
